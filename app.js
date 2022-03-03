404ac44dfe4331548b206f2ec08803d9f17093c4
rashida-otunba <rashidaotunba@gmail.com>
2022-03-02 19:12:13 -0500

    authorization and validation added

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const dotenv = require("dotenv");
+var jwt = require("jsonwebtoken");
+//get config vars; config method is expecting a env
+dotenv.config();
+const secretKey = process.env.SECRET_KEY;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+console.log("Salt rounds are:", process.env.SALT_ROUNDS);
+//example of a key
+console.log("SECRET KEY IS", secretKey);
+
+//adds the token from the jwt jsonwebtoken
+const token = jwt.sign(
+  {
+    data: "Free the ducks",
+  },
+  secretKey,
+  { expiresIn: "1h" }
+);
+
+jwt.verify(
+  token,
+  secretKey,
+  function(err,decoded){
+    console.log("Decoded", decoded) //will show the decoded console.log 
+  }
+);
+
+console.log("supersecret token is: ", token);
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
+const saltRounds=process.env.SALT_ROUNDS;
 const Sequelize = require("sequelize");
 const { User } = require("../models");
 
@@ -11,16 +12,16 @@ router.get("/", async function (req, res, next) {
   // console.log(users);
   // res.json(users);
   //render the file based on the view engine
-  res.render('index');
+  res.render("index");
 });
 
 router.post("/register", async (req, res, next) => {
   let { username, password, email } = req.body;
-  // const saltRounds = bcrypt.genSaltSync(5);
-  // const hash = bcrypt.hashSync(password, saltRounds);
-  // console.log(username, password, email); 
+  const saltRounds = bcrypt.genSaltSync(5);
+  const hash = bcrypt.hashSync(password, saltRounds);
+  console.log(username, password, email);
 
-  const newUser = await User.create({
+  const newUser = await User.create({ //awaiting for the user input 
     username: username,
     password: hash,
     email: email,
@@ -33,45 +34,49 @@ router.post("/register", async (req, res, next) => {
   res.send("User add");
 });
 
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
 
-// router.post("/register", (req, res, next) => {
-//   const password = "hello";
+  
 
-//   const saltRounds = bcrypt.genSaltSync(5);
-//   //We don’t have to now but we should also put the salt # into a .env file
-//   const hash = bcrypt.hashSync(password, saltRounds);
+  const saltRounds = bcrypt.genSaltSync(5);
+  const hash = bcrypt.hashSync(password, saltRounds);
+  
 
-//   console.log("my password: ", password);
-//   console.log("my hashed password: ", hash);
-
-//   // bcrypt.hash(myPassword, saltRounds, (err, hash) => {});
-
-//   res.send("User add");
-// });
-
-router.post("/login", async(req, res, next) => {
-
-  const  {username, password}=req.body;
-
-  // const saltRounds = bcrypt.genSaltSync(5);
-  // const hash = bcrypt.hashSync(password, saltRounds);
-  // const comparePass = bcrypt.compareSync(password, hash);
-
-  //attach to database 
+  //attach to database
   const users = await User.findOne({
-    where:{
-      username: username, 
-    }
+    where: {
+      username: username,
+    },
   }); //referencing the model name from the models folder
 
-  res.json(users)
 
-    // console.log("my password: ", password);
+  console.log(users);
+
+  const dbPassword = users.password; //coming from the database
+  const comparePass = bcrypt.compareSync(password, dbPassword); //comparing hash password to db password 
+//compare will return false ; bcrypt is running a comparison and is taking a plain text and hasing it and comparing it 
+
+  if (comparePass) {
+   res.render('NEXT EJS/HTML PAGE ')
+    // res.json(users); //this will return all user data and send to JSON in the browser
+    res.send("YOU ARE AUTHORIZED ")
+    console.log("true - authorized user found");
+  } else {
+    console.log("no user found");
+    res.send("Not authorized") //will appear in browser 
+  }
+
+  console.log(password);
+  console.log(dbPassword);
+
+  // console.log("my password: ", password);
   // console.log("my hashed password: ", hash);
   // console.log("is password correct: ", comparePass);
   // console.log("is wrong password correct: ", wrongComparePass);
-
-  console.log(users)
 });
+
+
+
 
 module.exports = router;
